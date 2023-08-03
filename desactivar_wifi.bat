9e4d3764d6d6bea3af55cf109fde18317dd55999
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:01:36 -0300

    Update desactivar_wifi.bat

--- desactivar_wifi.bat
+++ desactivar_wifi.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Desactivar el Wi-Fi --
 echo Deshabilitando el Wi-Fi...
 for /F "tokens=2 delims=: " %%G in ('netsh wlan show interface ^| findstr /C:"Nombre de la interfaz"') do set "nombre_adaptador=%%G"
