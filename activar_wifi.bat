062bd8a85d8532eb85edc6b6c29392a0bc359f45
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:01:10 -0300

    Update activar_wifi.bat

--- activar_wifi.bat
+++ activar_wifi.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Activar el Wi-Fi --
 echo Habilitando el Wi-Fi...
 for /F "tokens=2 delims=: " %%G in ('netsh wlan show interface ^| findstr /C:"Nombre de la interfaz"') do set "nombre_adaptador=%%G"
