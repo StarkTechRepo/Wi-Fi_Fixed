bb10c4d0d00f22eeb44d9b2236ba21b854d31380
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:01:47 -0300

    Update diagnosticar_wifi.bat

--- diagnosticar_wifi.bat
+++ diagnosticar_wifi.bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Ejecutar el solucionador de problemas de Wi-Fi --
 echo Ejecutando el solucionador de problemas de Wi-Fi...
 msdt.exe /id NetworkDiagnosticsNetworkAdapter
