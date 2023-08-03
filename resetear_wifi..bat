b95280a5882a5f14bbb80e8e3205cad6914a241f
OtaconEvil <otaconeviloficial@gmail.com>
2023-08-03 17:02:11 -0300

    Update resetear_wifi..bat

--- resetear_wifi..bat
+++ resetear_wifi..bat
@@ -1,4 +1,6 @@
 @echo off
+title - https://github.com/OtaconEvil
+
 REM -- Reiniciar la configuración de la tarjeta Wi-Fi --
 echo Reiniciando la configuración de la tarjeta Wi-Fi...
 netsh int ip reset resetlog.txt
